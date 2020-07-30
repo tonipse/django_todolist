44572d2f1742064b638c9c9918732078c27198ef
Toni Peise <toni.peise@gmx.net>
2020-07-30 03:01:40 +0200

    Heroku v5

--- todo/settings.py
+++ todo/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'w-w*^_^jvad)blkl(1fnhgvdhkq+p$r#7-sbo3$52ybku!zfmv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['todolisttoni.herokuapp.com']
 
 # Application definition
 
